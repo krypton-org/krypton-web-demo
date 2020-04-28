f312ed765e839125af7d66d25caa4022cc48b039
jrebecchi <johann.corcuff@gmail.com>
2020-04-29 01:31:24 +0200

    fixing warnings

--- src/components/partials/NavBar.tsx
+++ src/components/partials/NavBar.tsx
@@ -78,18 +78,6 @@ class NavBar extends Component<Props & RouteComponentProps, State> {
                                     alt="krypton web logo"
                                 />
                             </a>
-                            {/* eslint-disable-next-line */}
-                            <a
-                                role="button"
-                                className="navbar-burger burger"
-                                aria-label="menu"
-                                aria-expanded="false"
-                                data-target="navbarBasicExample"
-                            >
-                                <span aria-hidden="true"></span>
-                                <span aria-hidden="true"></span>
-                                <span aria-hidden="true"></span>
-                            </a>
                         </div>
                         <div id="navbarBasicExample" className="navbar-menu">
                             <div className="navbar-start">
